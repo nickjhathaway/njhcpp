8fc3b5658e85be83df2f2b15a2bd3a9d511ea934
Nicholas Hathaway <nickjhathaway@gmail.com>
2014-11-17 11:28:55 -0500

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 
 using namespace bib;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
   std::cout << bashCT::bold << "Hello bibcpp!" << bashCT::reset << std::endl;
   return 0;
 }
