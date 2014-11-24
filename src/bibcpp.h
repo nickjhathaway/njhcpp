48bb57890f56245c635fd62af55c223071d9f1fc
Nicholas Hathaway <nickjhathaway@gmail.com>
2014-11-23 19:42:26 -0500

    added some json utils for bibcpp for converting some std library objects to json

--- src/bibcpp.h
+++ src/bibcpp.h
@@ -3,6 +3,7 @@
 #include "bibcpp/stdAddition.h"
 #include "bibcpp/bashUtils.h"
 #include "bibcpp/graphics.h"
+#include "bibcpp/jsonUtils.h"
 #include "bibcpp/utils.h"
 #include "bibcpp/progutils.h"
 #include "bibcpp/simulation.h"
