cddd1f36c8865bf6b197f04c8d079a8cd2e9f828
Nicholas Hathaway <nickjhathaway@gmail.com>
2016-09-05 14:34:45 -0400

    allowing indentation of scoped message;

--- src/bibcpp/debug/scopedMessage.hpp
+++ src/bibcpp/debug/scopedMessage.hpp
@@ -22,11 +22,17 @@ public:
 	 * @param out Where to print messages
 	 * @param debug A debug flag to turn off printing to make it easy to turn off messaging
 	 */
-	scopedMessage(const std::string & start,
-			const std::string & end, std::ostream & out,
-			bool debug): startMessage_(start),
-					endMessage_(end), out_(out), debug_(debug) {
-		if(debug_) out_ << bib::bashCT::green << startMessage_ << bib::bashCT::reset << std::endl;
+	scopedMessage(const std::string & start, const std::string & end,
+			std::ostream & out, bool debug, uint32_t indentLvl = 0) :
+			startMessage_(start), endMessage_(end), out_(out), debug_(debug), indentLvl_(
+					indentLvl) {
+		if (debug_) {
+			if (indentLvl_ > 0) {
+				out_ << std::string(indentLvl_, '\t');
+			}
+			out_ << bib::bashCT::green << startMessage_ << bib::bashCT::reset
+					<< std::endl;
+		}
 	}
 	/**@brief Constructor with start message and end message is start_end message
 	 *
@@ -34,10 +40,17 @@ public:
 	 * @param out Where to print messages
 	 * @param debug A debug flag to turn off printing to make it easy to turn off messaging
 	 */
-	scopedMessage(const std::string & start,
-			std::ostream & out, bool debug): startMessage_(start),
-					endMessage_(start + "_stop"), out_(out), debug_(debug) {
-		if(debug_) out_ << bib::bashCT::green << startMessage_ << bib::bashCT::reset << std::endl;
+	scopedMessage(const std::string & start, std::ostream & out, bool debug,
+			uint32_t indentLvl = 0) :
+			startMessage_(start), endMessage_(start + "_stop"), out_(out), debug_(
+					debug), indentLvl_(indentLvl) {
+		if (debug_) {
+			if (indentLvl_ > 0) {
+				out_ << std::string(indentLvl_, '\t');
+			}
+			out_ << bib::bashCT::green << startMessage_ << bib::bashCT::reset
+					<< std::endl;
+		}
 	}
 
 	//members
@@ -46,12 +59,19 @@ public:
 	std::ostream & out_; /**< Where to print messages */
 	bool debug_; /**< Debug flag to turn off printing  */
 
+	uint32_t indentLvl_ = 0 /**< How much to indent for the message */;
+
 	/**@brief destructor with print message
 	 *
 	 * print the end message for when the obj goes out of scope
 	 */
 	~scopedMessage(){
-		if(debug_) out_ << bib::bashCT::red << endMessage_ << bib::bashCT::reset << std::endl;
+		if(debug_){
+			if(indentLvl_ > 0){
+				out_ << std::string(indentLvl_, '\t');
+			}
+			out_ << bib::bashCT::blue << endMessage_ << bib::bashCT::reset << std::endl;
+		}
 	}
 };
 
