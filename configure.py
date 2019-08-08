2159d4d05c23ccc7aa670ef254ce2a05ae7f5a7f
Nick Hathaway <nickjhathaway@gmail.com>
2019-08-08 13:44:29 -0700

    no message

--- configure.py
+++ configure.py
@@ -8,7 +8,7 @@ from genFuncs import genHelper
 def main():
     name = "njhcpp"
     #libs = "boost_filesystem:1_68_0,pstreams:RELEASE_1_0_1,jsoncpp:1.8.3,cppitertools:v0.2.1,zlib:1.2.11"
-    libs = "boost:1_70_0,pstreams:RELEASE_1_0_1,jsoncpp:1.8.3,cppitertools:v0.2.1,zlib:1.2.11"
+    libs = "boost_filesystem:develop,pstreams:RELEASE_1_0_1,jsoncpp:1.8.3,cppitertools:v0.2.1,zlib:1.2.11"
     args = genHelper.parseNjhConfigureArgs()
     if Utils.isMac():
         cmd = genHelper.mkConfigCmd(name, libs, sys.argv, "-lpthread,-lz")
